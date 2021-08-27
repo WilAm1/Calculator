c33714733a719f687e47c99f42d3292df435eb1b
Wil <wilhelmjanmapogi@gmail.com>
2021-08-27 09:50:16 +0800

    Add storage of input

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-const
+function assignEventListener() {
+    const aAllBtns = document.querySelectorAll('.btn');
+
+    aAllBtns.forEach(btn => {
+        btn.addEventListener('click', e => {
+            storeValue(e.target.textContent);
+        })
+    });
+}
+
+function storeValue(value) {
+    sUserInput += value;
+    console.log(sUserInput);
+}
+let sUserInput = '';
+
+assignEventListener();
