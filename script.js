fa3cb193a0766c0ee21ffdd33f82788ae6d28682
Wil <wilhelmjanmapogi@gmail.com>
2021-08-27 10:54:41 +0800

    Move text content up

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-function assignDisplayableEventListener() {
-    const aAllBtns = document.querySelectorAll('.btn-display');
+function assignNumEventListeners() {
+    const aAllBtns = document.querySelectorAll('.num-val');
 
     aAllBtns.forEach(btn => {
         btn.addEventListener('click', e => {
             const value = e.target.textContent;
             storeValue(value);
+
             changeInputDisplay()
         })
     });
 }
 
+function changeInputDisplay() {
+    const inputTextP = document.getElementById('input-text');
+    inputTextP.textContent = sUserInput;
+}
 
 function storeValue(value) {
     sUserInput += value;
@@ -17,9 +22,24 @@ function storeValue(value) {
 }
 let sUserInput = '';
 
-function changeInputDisplay() {
-    const inputTextP = document.getElementById('input-text');
-    inputTextP.textContent = sUserInput;
-}
+const aOperatorBtns = document.querySelectorAll('.btn-operator');
+const compPara = document.getElementById('computation');
 
-assignDisplayableEventListener();
+aOperatorBtns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        compPara.textContent = sUserInput + ' ' + e.target.textContent;
+        console.log(sUserInput + ' ' + e.target.textContent);
+        sUserInput = '';
+        changeInputDisplay()
+    });
+});
+
+assignNumEventListeners();
+
+
+// Display clicked btn
+// If operator found, move the text content up to the display.
+// Clear the input text in order for user to input again.
+// First operator then evaluate the numbers when equal is called
+// Make operators Work. IF 2 operators found, process the first operator
+//
