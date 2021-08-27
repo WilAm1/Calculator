ebe5942de4d199d717efe7f4a414139c1f0236a4
Wil <wilhelmjanmapogi@gmail.com>
2021-08-27 17:32:33 +0800

    implement second num recoginizer

--- script.js
+++ script.js
@@ -2,17 +2,57 @@
 
 const btnDisplayableArr = Array.from(document.querySelectorAll('.btn-display'));
 console.log(btnDisplayableArr);
-const inputText = document.querySelector('#input-text');
+const inputTextDOM = document.querySelector('#input-text');
 
 // Display Input into the input Paragraph
 
-let initialNum = ''
+function displayInitExp(num) {
+    if (!isSecondNum) {
+        initialNum += num;
+        inputTextDOM.textContent = initialNum;
+        console.log(initialNum)
+
+    } else {
+        storedNum += num;
+        inputTextDOM.textContent = storedNum;
+        console.log(storedNum);
+    }
+}
+
+function displayCompExp(operator) {
+    const computationDOM = document.querySelector('#computation');
+    // storedNum = initialNum;
+    compExpression = initialNum + operator;
+    computationDOM.textContent = compExpression;
+    // initialNum = '';
+
+}
+
+let initialNum = '';
+let isSecondNum = false;
+let storedNum = '';
+let operatorValue = '';
+let compExpression = '';
+let result = 0;
 btnDisplayableArr.forEach(btn => {
     btn.addEventListener('click', e => {
-        initialNum += e.target.textContent;
-        inputText.textContent = initialNum;
+        displayInitExp(e.target.textContent);
     })
 });
 
 // If operator is found, move it to computation p
+
+const btnOperators = Array.from(document.querySelectorAll('.btn-operator'));
+console.log(btnOperators)
+
+btnOperators.forEach(btn => {
+    btn.addEventListener('click', e => {
+        operatorValue = e.target.textContent;
+        displayCompExp(operatorValue);
+        console.log(operatorValue);
+        isSecondNum = true;
+        // acceptNext
+
+    })
+});
 // make input P displayable again.
